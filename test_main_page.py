0905259a2573695c236a1c92e8cc4c8489a24e79
Kirill Burov <burov.kian@gmail.com>
2025-02-09 19:34:46 +0400

    step 4 2 4
    Создан рабочий метод, который инициализирует Page Object, открывает страницу и выполняет метод страницы из main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -2,18 +2,10 @@ import pytest,time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-
-link = "http://selenium1py.pythonanywhere.com/"
-# def test_guest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     browser.get(link)
-#     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-#     login_link.click()
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
-   browser.get(link)
-   go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
