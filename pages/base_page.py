c5a6c072f87615d349262d9210e602f1385aa71f
Kirill Burov <burov.kian@gmail.com>
2025-02-10 05:28:42 +0400

    step 4 3 8

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
+from .locators import BasePageLocators
 class BasePage():
     def __init__(self, browser, url,timeout=10):
         self.browser = browser
@@ -36,6 +37,13 @@ class BasePage():
             return False
 
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
--- test_product_page.py
+++ test_product_page.py
@@ -7,7 +7,7 @@ from .pages.product_page import ProductPage
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
+urls = [f"{product_base_link}/?promo=offer{no}" for no in range(2)]
 
 
 @pytest.mark.xfail
@@ -39,3 +39,15 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.should_be_added_to_basket()
     page.should_be_disappeared_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
